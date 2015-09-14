aea072709e0226350880f6a296c72dacf0ec1e28
Ramses Leal <leal.ramses@gmail.com>
2015-09-14 18:34:51 +0200

    Update 14/09/2015

--- AiLinux.sh
+++ AiLinux.sh
@@ -2,12 +2,12 @@
 # Función que realiza las tareas pertinentes.
 function ailinux(){
   # Variables.
-  proc=9
+  let proc=9
   s_null="/dev/null"
   current_user=$(whoami)
   # Texto de introducción.
   echo "================================================================================"
-  echo -e " Bienvenido \e[1;34m"$current_user"\e[0m - Script de instalación de recursos básicos tras la "
+  echo -e " Bienvenido \e[1;34m"$current_user"\e[0m - Script Bash de instalación de recursos básicos tras la "
   echo " instalación de Sistemas Operativos GNU/Linux basados en Debian (Ubuntu - Mint) "
   echo "================================================================================"
   # Indicamos al usuario que comenzamos las tareas.
@@ -15,22 +15,49 @@ function ailinux(){
   echo "[Realizando tareas, espere un momento por favor...]"
   echo
   echo "Procesos restantes: "$proc
-  echo
   # Tareas.
+  #
   add-apt-repository -y ppa:tualatrix/ppa &> $s_null #
-  proc=$proc-1
+  let "proc -= 1"
+  echo "Procesos restantes: "$proc
+  #
   apt-get update &> $s_null #
+  let "proc -= 1"
+  echo "Procesos restantes: "$proc
+  #
   apt-get upgrade -y &> $s_null #
+  let "proc -= 1"
+  echo "Procesos restantes: "$proc
+  #
   apt install ubuntu-restricted-extras -y &> $s_null #
+  let "proc -= 1"
+  echo "Procesos restantes: "$proc
+  #
   apt install rar unace p7zip-full p7zip-rar sharutils mpack arj &> $s_null #
+  let "proc -= 1"
+  echo "Procesos restantes: "$proc
+  #
   apt install synaptic &> $s_null #
+  let "proc -= 1"
+  echo "Procesos restantes: "$proc
+  #
   apt install aptitude &> $s_null #
+  let "proc -= 1"
+  echo "Procesos restantes: "$proc
+  #
   apt install ubuntu-tweak &> $s_null #
+  let "proc -= 1"
+  echo "Procesos restantes: "$proc
+  #
   apt-get autoremove -y &> $s_null #
+  let "proc -= 1"
+  echo "Procesos restantes: "$proc
   # Indicamos al usuario que hemos finalizado las tareas.
+  echo
   echo "[Tareas finalizadas con éxito]"
   echo
   # Liberamos Variables.
+  proc=
   s_null=
   current_user=
 }
